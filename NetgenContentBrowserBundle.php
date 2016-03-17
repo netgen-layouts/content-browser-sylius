de9ee7c3feaf04542490f80dfeaaa0a5e83b32e6
Edi Modrić <edi.modric@gmail.com>
2016-03-17 14:36:49 +0100

    Refactor converters to only inject the currently selected converter

--- NetgenContentBrowserBundle.php
+++ NetgenContentBrowserBundle.php
@@ -3,20 +3,7 @@
 namespace Netgen\Bundle\ContentBrowserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ItemBuilderCompilerPass;
 
 class NetgenContentBrowserBundle extends Bundle
 {
-    /**
-     * Builds the bundle.
-     *
-     * It is only ever called once when the cache is empty.
-     *
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container A ContainerBuilder instance
-     */
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new ItemBuilderCompilerPass());
-    }
 }
