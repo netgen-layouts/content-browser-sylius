e4b248396301085ce1ca2e765140d9bf6b4a0c90
Edi Modrić <edi.modric@gmail.com>
2016-03-18 16:27:07 +0100

    Implement config loaders

--- NetgenContentBrowserBundle.php
+++ NetgenContentBrowserBundle.php
@@ -3,7 +3,20 @@
 namespace Netgen\Bundle\ContentBrowserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ChainedConfigLoaderPass;
 
 class NetgenContentBrowserBundle extends Bundle
 {
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ChainedConfigLoaderPass());
+    }
 }
