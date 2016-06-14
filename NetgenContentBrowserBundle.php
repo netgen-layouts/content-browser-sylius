ac2a1beb6507cd5dc1d132b69884a5a871a0c45d
Edi Modrić <edi.modric@gmail.com>
2016-06-14 22:56:19 +0200

    Major refactoring

--- NetgenContentBrowserBundle.php
+++ NetgenContentBrowserBundle.php
@@ -4,6 +4,11 @@ namespace Netgen\Bundle\ContentBrowserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\BackendRegistryPass;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ValueLoaderRegistryPass;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ColumnProviderPass;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ItemBuilderPass;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ItemRendererPass;
 use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ChainedConfigLoaderPass;
 
 class NetgenContentBrowserBundle extends Bundle
@@ -18,5 +23,10 @@ class NetgenContentBrowserBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new ChainedConfigLoaderPass());
+        $container->addCompilerPass(new BackendRegistryPass());
+        $container->addCompilerPass(new ValueLoaderRegistryPass());
+        $container->addCompilerPass(new ItemBuilderPass());
+        $container->addCompilerPass(new ItemRendererPass());
+        $container->addCompilerPass(new ColumnProviderPass());
     }
 }
