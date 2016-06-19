6d9c7e3109f77e444c7011a777aa1ff1441d832c
Edi Modrić <edi.modric@gmail.com>
2016-06-19 22:43:06 +0200

    Refactor value out of the item to allow having items with no values (for categories for example)

--- NetgenContentBrowserBundle.php
+++ NetgenContentBrowserBundle.php
@@ -5,9 +5,8 @@ namespace Netgen\Bundle\ContentBrowserBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\BackendRegistryPass;
-use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ValueLoaderRegistryPass;
 use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ColumnProviderPass;
-use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ItemBuilderPass;
+use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ItemConfiguratorPass;
 use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ItemRendererPass;
 use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\CompilerPass\ChainedConfigLoaderPass;
 
@@ -24,8 +23,7 @@ class NetgenContentBrowserBundle extends Bundle
     {
         $container->addCompilerPass(new ChainedConfigLoaderPass());
         $container->addCompilerPass(new BackendRegistryPass());
-        $container->addCompilerPass(new ValueLoaderRegistryPass());
-        $container->addCompilerPass(new ItemBuilderPass());
+        $container->addCompilerPass(new ItemConfiguratorPass());
         $container->addCompilerPass(new ItemRendererPass());
         $container->addCompilerPass(new ColumnProviderPass());
     }
