343065471e3d221cbd1621f7b395ebadaae2f377
Edi Modrić <edi.modric@gmail.com>
2018-06-28 19:09:55 +0200

    Remove obsolete DI extension tests

--- tests/bundle/DependencyInjection/NetgenContentBrowserExtensionTest.php
+++ tests/bundle/DependencyInjection/NetgenContentBrowserExtensionTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Netgen\Bundle\ContentBrowserBundle\Tests\DependencyInjection;
 
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
-use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\Configuration;
 use Netgen\Bundle\ContentBrowserBundle\DependencyInjection\NetgenContentBrowserExtension;
 
 final class NetgenContentBrowserExtensionTest extends AbstractExtensionTestCase
@@ -23,69 +22,6 @@ final class NetgenContentBrowserExtensionTest extends AbstractExtensionTestCase
         $extension = $this->container->getExtension('netgen_content_browser');
 
         $this->extension = $extension;
-    }
-
-    /**
-     * We test for existence of one service from each of the config files.
-     *
-     * @covers \Netgen\Bundle\ContentBrowserBundle\DependencyInjection\NetgenContentBrowserExtension::load
-     */
-    public function testServices(): void
-    {
-        $this->container->setParameter(
-            'kernel.bundles',
-            [
-                'EzPublishCoreBundle' => 'EzPublishCoreBundle',
-                'NetgenTagsBundle' => 'NetgenTagsBundle',
-                'SyliusCoreBundle' => 'SyliusCoreBundle',
-            ]
-        );
-
-        $this->load(
-            [
-                'item_types' => [
-                    'ezcontent' => [
-                        'name' => 'item_types.ezcontent',
-                        'preview' => [
-                            'template' => 'template.html.twig',
-                        ],
-                    ],
-                ],
-            ]
-        );
-
-        $this->assertContainerBuilderHasParameter(
-            'netgen_content_browser.item_types',
-            [
-                'ezcontent' => 'item_types.ezcontent',
-            ]
-        );
-
-        $this->assertContainerBuilderHasService('netgen_content_browser.backend.ezlocation');
-        $this->assertContainerBuilderHasService('netgen_content_browser.backend.ezcontent');
-        $this->assertContainerBuilderHasService('netgen_content_browser.backend.eztags');
-        $this->assertContainerBuilderHasService('netgen_content_browser.backend.sylius_product');
-
-        $this->assertContainerBuilderHasSyntheticService('netgen_content_browser.current_config');
-    }
-
-    /**
-     * We test for existence of one service from each of the config files.
-     *
-     * @covers \Netgen\Bundle\ContentBrowserBundle\DependencyInjection\NetgenContentBrowserExtension::load
-     */
-    public function testServicesWithoutBundles(): void
-    {
-        $this->container->setParameter('kernel.bundles', []);
-
-        $this->load();
-
-        $this->assertContainerBuilderNotHasService('netgen_content_browser.backend.ezlocation');
-        $this->assertContainerBuilderNotHasService('netgen_content_browser.backend.ezcontent');
-        $this->assertContainerBuilderNotHasService('netgen_content_browser.backend.eztags');
-        $this->assertContainerBuilderNotHasService('netgen_content_browser.backend.sylius_product');
-
-        $this->assertContainerBuilderHasSyntheticService('netgen_content_browser.current_config');
     }
 
     /**
@@ -109,48 +45,6 @@ final class NetgenContentBrowserExtensionTest extends AbstractExtensionTestCase
                 ],
             ]
         );
-    }
-
-    /**
-     * @covers \Netgen\Bundle\ContentBrowserBundle\DependencyInjection\NetgenContentBrowserExtension::getConfiguration
-     */
-    public function testGetConfiguration(): void
-    {
-        $configuration = $this->extension->getConfiguration([], $this->container);
-        $this->assertInstanceOf(Configuration::class, $configuration);
-    }
-
-    /**
-     * We test for existence of one config value from each of the config files.
-     *
-     * @covers \Netgen\Bundle\ContentBrowserBundle\DependencyInjection\NetgenContentBrowserExtension::doPrepend
-     * @covers \Netgen\Bundle\ContentBrowserBundle\DependencyInjection\NetgenContentBrowserExtension::prepend
-     */
-    public function testPrepend(): void
-    {
-        $this->container->setParameter(
-            'kernel.bundles',
-            [
-                'EzPublishCoreBundle' => 'EzPublishCoreBundle',
-                'NetgenTagsBundle' => 'NetgenTagsBundle',
-                'SyliusCoreBundle' => 'SyliusCoreBundle',
-            ]
-        );
-
-        $this->extension->prepend($this->container);
-
-        $config = call_user_func_array(
-            'array_merge_recursive',
-            $this->container->getExtensionConfig('netgen_content_browser')
-        );
-
-        $this->assertInternalType('array', $config);
-        $this->assertArrayHasKey('item_types', $config);
-
-        $this->assertArrayHasKey('ezcontent', $config['item_types']);
-        $this->assertArrayHasKey('ezlocation', $config['item_types']);
-        $this->assertArrayHasKey('eztags', $config['item_types']);
-        $this->assertArrayHasKey('sylius_product', $config['item_types']);
     }
 
     protected function getContainerExtensions(): array
