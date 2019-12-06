9e9c7f044a9658174fa7ad6d97b4aeb6504a6d22
Edi Modrić <edi.modric@gmail.com>
2019-12-06 15:30:33 +0100

    Fix outstanding issues with PHPStan

--- lib/Repository/TaxonRepositoryInterface.php
+++ lib/Repository/TaxonRepositoryInterface.php
@@ -11,11 +11,15 @@ interface TaxonRepositoryInterface extends BaseTaxonRepositoryInterface
 {
     /**
      * Creates a paginator which is used to filter taxons.
+     *
+     * @return \Pagerfanta\Pagerfanta<\Sylius\Component\Taxonomy\Model\TaxonInterface>
      */
     public function createListPaginator(string $parentCode, string $localeCode): Pagerfanta;
 
     /**
      * Creates a paginator which is used to search for taxons.
+     *
+     * @return \Pagerfanta\Pagerfanta<\Sylius\Component\Taxonomy\Model\TaxonInterface>
      */
     public function createSearchPaginator(string $searchText, string $localeCode): Pagerfanta;
 }
