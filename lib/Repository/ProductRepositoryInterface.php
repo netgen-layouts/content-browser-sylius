eb6918935ea5e498f49291cd999a7aa2e6320407
Edi Modrić <edi.modric@gmail.com>
2019-12-09 13:58:05 +0100

    Do not use generics with Pagerfanta

--- lib/Repository/ProductRepositoryInterface.php
+++ lib/Repository/ProductRepositoryInterface.php
@@ -12,15 +12,11 @@ interface ProductRepositoryInterface extends BaseProductRepositoryInterface
 {
     /**
      * Creates a paginator which is used to filter products by taxon.
-     *
-     * @return \Pagerfanta\Pagerfanta<\Sylius\Component\Product\Model\ProductInterface>
      */
     public function createByTaxonPaginator(TaxonInterface $taxon, string $localeCode): Pagerfanta;
 
     /**
      * Creates a paginator which is used to search for products.
-     *
-     * @return \Pagerfanta\Pagerfanta<\Sylius\Component\Product\Model\ProductInterface>
      */
     public function createSearchPaginator(string $searchText, string $localeCode): Pagerfanta;
 }
