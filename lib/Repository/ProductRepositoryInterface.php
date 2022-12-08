a648a238cf43c42987ab83346f4461355bfa013d
Edi Modrić <edi.modric@gmail.com>
2022-12-08 15:04:49 +0100

    Fix PHPStan error

--- lib/Repository/ProductRepositoryInterface.php
+++ lib/Repository/ProductRepositoryInterface.php
@@ -12,11 +12,15 @@ interface ProductRepositoryInterface extends BaseProductRepositoryInterface
 {
     /**
      * Creates a paginator which is used to filter products by taxon.
+     *
+     * @return \Pagerfanta\PagerfantaInterface<\Sylius\Component\Product\Model\ProductInterface>
      */
     public function createByTaxonPaginator(TaxonInterface $taxon, string $localeCode): PagerfantaInterface;
 
     /**
      * Creates a paginator which is used to search for products.
+     *
+     * @return \Pagerfanta\PagerfantaInterface<\Sylius\Component\Product\Model\ProductInterface>
      */
     public function createSearchPaginator(string $searchText, string $localeCode): PagerfantaInterface;
 }
--- lib/Repository/TaxonRepositoryInterface.php
+++ lib/Repository/TaxonRepositoryInterface.php
@@ -11,11 +11,15 @@ interface TaxonRepositoryInterface extends BaseTaxonRepositoryInterface
 {
     /**
      * Creates a paginator which is used to filter taxons.
+     *
+     * @return \Pagerfanta\PagerfantaInterface<\Sylius\Component\Taxonomy\Model\TaxonInterface>
      */
     public function createListPaginator(string $parentCode, string $localeCode): PagerfantaInterface;
 
     /**
      * Creates a paginator which is used to search for taxons.
+     *
+     * @return \Pagerfanta\PagerfantaInterface<\Sylius\Component\Taxonomy\Model\TaxonInterface>
      */
     public function createSearchPaginator(string $searchText, string $localeCode): PagerfantaInterface;
 }
